19e0f88d6834cdac9ba0682606e35873a9b44dac
François Diaz-Maurin <francois.diazmaurin@gmail.com>
2020-05-15 10:18:30 +0200

    Added ggparcoord plots

---
+++ parcoord-coalitions.R
@@ -0,0 +1,34 @@
+# ====== Packages ======
+required_packages <- c("hrbrthemes",
+                       "GGally",
+                       "data.table",
+                       "viridis")
+
+uninstalled_packages <- required_packages[!(required_packages %in% installed.packages()[,'Package'])]
+if (length(uninstalled_packages) > 0) {
+  install.packages(uninstalled_packages, dependencies = TRUE, repos = 'http://cran.us.r-project.org')
+}
+
+lapply(required_packages, require, character.only = TRUE)
+
+rm(required_packages)
+rm(uninstalled_packages)
+
+# Data set
+data <- read.csv("coalitions.csv", colClasses=c(rep('numeric', 4), 'factor'))
+
+# Plot
+ggparcoord(data,
+    columns = 1:4, groupColumn = 5, order = "allClass",
+    scale="globalminmax",
+    showPoints = TRUE, 
+    title = "Ranking by coalitions",
+    alphaLines = 1
+    ) + 
+  scale_color_viridis(discrete=TRUE) +
+  theme_ipsum()+
+  theme(
+    plot.title = element_text(size=10),
+    axis.title.x= element_blank(),
+    axis.title.y= element_blank()
+  )
